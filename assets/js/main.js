d2d2d681d8dbe87a7893fb7c17c213856dbc5be9
Tajbir Hossain <mdtajbirhossain031@gmail.com>
2024-02-03 18:23:17 +0600

    jasdfl

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,8 @@ overlay.addEventListener("click", () => {
 const popupFormWrap = document.querySelector(".popupFormWrap")
 const popupOverlay = document.querySelector(".popupOverlay")
 const pricingBuyBtn = document.querySelectorAll(".pricingMainCard button")
+const networkRowLeft = document.querySelectorAll(".networkRowLeft")
+const networkRowRight = document.querySelectorAll(".networkRowRight")
 
 
 pricingBuyBtn.forEach(item => {
@@ -28,6 +30,21 @@ pricingBuyBtn.forEach(item => {
     })
 })
 
+
+networkRowLeft.forEach(item => {
+    item.addEventListener("click", () => {
+        popupFormWrap.classList.add("active")
+    })
+})
+networkRowRight.forEach(item => {
+    item.addEventListener("click", () => {
+        popupFormWrap.classList.add("active")
+    })
+})
+
+
 popupOverlay.addEventListener("click", () => {
     popupFormWrap.classList.remove("active")
 })
+
+
