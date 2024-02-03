27f78ffd23da6db7c6de5c95f477134dfec9bbaf
Tajbir Hossain <mdtajbirhossain031@gmail.com>
2024-02-03 17:25:23 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const bars = document.querySelector(".fa-bars")
+const menu = document.querySelector(".headerRight ul")
+const overlay = document.querySelector(".overlay")
+
+
+bars.addEventListener("click", () => {
+    menu.classList.add("active")
+    overlay.classList.add("active")
+})
+overlay.addEventListener("click", () => {
+    menu.classList.remove("active")
+    overlay.classList.remove("active")
+})
